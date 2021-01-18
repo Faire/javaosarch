dfd7a6f5e52a5b80aed2ce1153efc8a8744653ef
Alen Komljen <alen.komljen@live.com>
2021-01-18 16:25:37 +0100

    Initial commit

---
+++ src/main/java/com/faire/blog/javaosarch/JavaOsArch.java
@@ -0,0 +1,10 @@
+package javaosarch;
+
+public class JavaOsArch {
+
+    public static void main(final String[] args) {
+        System.out.println("OS name : "+System.getProperty("os.name"));
+        System.out.println("OS arch : "+System.getProperty("os.arch"));
+    }
+
+}
